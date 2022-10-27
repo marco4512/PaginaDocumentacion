c999020cf892205321ee181d70b569011a48822a
Marco <marco_alex45@hotmail.com>
2022-10-27 17:38:30 -0500

    desplazando tabla

---
+++ js/desplazar.js
@@ -0,0 +1,13 @@
+console.log('cargando')
+var el = document.getElementById("boot_flecha");
+var a = document.getElementById("objeto");
+el.addEventListener("click", function(){
+    console.log('se oprimio div')
+    
+    }, false);
+
+    a.addEventListener("click", function(){
+        console.log('se oprimio o')
+        
+        }, false);
+    
